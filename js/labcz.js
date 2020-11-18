8e587c6fe99a2930597dd64cb1f6b1171e3878aa
Labcz <251698219@qq.com>
2020-11-18 18:56:07 +0800

    Site updated: 2020-11-18 18:56:07

---
+++ js/labcz.js
@@ -0,0 +1,4 @@
+// 底部版权
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Labcz');
+});
